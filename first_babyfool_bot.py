c80cf4233f3f51868a0711cd525de4fe55e5468c
Vita Kosinets <v.kosinets17@gmail.com>
2024-10-04 18:03:03 +0300

    add simple answer to any updates

---
+++ first_babyfool_bot.py
@@ -0,0 +1,28 @@
+import requests
+import time
+
+
+API_URL = 'https://api.telegram.org/bot'
+BOT_TOKEN = '7772162688:AAEk0rqIvK0w7La94pxhRwxm05mcrFEt71c'
+TEXT = 'Спасибо, что пользуетесь мной! Когда-нибудь мой функционал позволит ответить вам'
+MAX_COUNTER = 100
+
+offset = -2
+counter = 0
+chat_id: int
+
+
+while counter < MAX_COUNTER:
+
+    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+
+    time.sleep(1)
+    counter += 1
